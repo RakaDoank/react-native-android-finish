2311fcb3fab296a077cde68519c1afd144ae318a
RakaDoank <rakaaudira@gmail.com>
2024-08-05 00:03:47 +0700

    remove comment

--- android/src/archnew/AndroidFinishSpec.kt
+++ android/src/archnew/AndroidFinishSpec.kt
@@ -2,7 +2,5 @@ package com.audira.lib.reactnative.androidfinish
 
 import com.facebook.react.bridge.ReactApplicationContext
 
-// import com.audira.lib.reactnative.androidfinish.NativeAndroidFinishSpec
-
 abstract class AndroidFinishSpec internal constructor(context: ReactApplicationContext) : NativeAndroidFinishSpec(context) {
 }
