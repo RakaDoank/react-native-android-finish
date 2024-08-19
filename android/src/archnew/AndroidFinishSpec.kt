5a3635f6ea2931f7a81177474a959edb1cdf6d55
RakaDoank <rakaaudira@gmail.com>
2024-08-19 21:28:53 +0700

    interoperate module name

--- android/src/archnew/AndroidFinishSpec.kt
+++ android/src/archnew/AndroidFinishSpec.kt
@@ -3,4 +3,9 @@ package com.audira.lib.reactnative.androidfinish
 import com.facebook.react.bridge.ReactApplicationContext
 
 abstract class AndroidFinishSpec internal constructor(context: ReactApplicationContext) : NativeAndroidFinishSpec(context) {
+
+	companion object {
+		const val NAME = NativeAndroidFinishSpec.NAME
+	}
+
 }
